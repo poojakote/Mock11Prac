17f0335ab349678255030705f5a90a8c65cec9cb
PKVel <43365063+poojakote@users.noreply.github.com>
2023-02-01 16:13:10 +0530

    Add files via upload

---
+++ GitPracFinal/GitPracFinal/src/prac2/GitFinal11.java
@@ -0,0 +1,12 @@
+package prac2;
+
+public class GitFinal11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Best Class");
+		System.out.println("Base Base");
+	}
+
+}
